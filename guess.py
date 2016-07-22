2620c7b92fb03ea80f4c5f91d6e5febbcb8fb699
Shobi <ppshobi@gmail.com>
2016-07-21 21:45:40 -0400

    Number Guessing agme modified

--- guess.py
+++ guess.py
@@ -1,23 +1,30 @@
 #!/usr/bin/python3
+#Guessing game
 
 import random
-
 secret=random.randint(1,10)
-
+print("**********************")
+print("Number Guessing Game")
+print("**********************")
+print("Give me Your Name")
+name=input()
+print("Welcome "+ name)
 for guesstaken in range(1,7):
-	print("Take a Guess!")
+	print("\nTake a Guess! "+ name)
 	guess=int(input())
 	if(guess<secret):
-		print("Your Guess is low")
+		print("Your Guess is low!")
 	elif(guess>secret):
-		print("Your Guess is high")
+		print("Your Guess is high!")
 	else:
 		break
 
 if(guess==secret):
-	print("Your Guess is correct" + str(guess))
+	print("***********You Win***********")
+	print("Your Guess is correct " + str(guess))
 	print("You Took "+str(guesstaken) +" Guesses")
 else:
+	print("***********You Lose**********")
 	print("You Took "+str(guesstaken) +" Guesses")
 	print("The Secret was "+ str(secret))
 	
